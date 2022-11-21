a99b4f0602362c7d7d918667a37ec5fd8007cd69
Marcos Coelho <mviniciusca@gmail.com>
2022-11-21 14:58:45 -0300

    first commit

--- bootcamp/course/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import courseindex, coursecreate
-
-urlpatterns = [
-    path('', courseindex, name='courseIndex'),
-    path('create', coursecreate, name="couseCreate")
-]
